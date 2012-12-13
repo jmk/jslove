2dfa10ff20ebdfc9d6e0800974aa7ed8f9eb7cf2
Jason Kim <jason@512k.org>
2012-12-13 00:22:07 -0800

    Add wrapper for setting min/mag filter for image.

--- src/modules/graphics/wrapGraphics_Image.cpp
+++ src/modules/graphics/wrapGraphics_Image.cpp
@@ -3,6 +3,8 @@
 #include <common/types.h>
 #include <graphics/opengl/Image.h>
 
+#include <string>
+
 using love::graphics::opengl::Image;
 
 DECLARE_CLASS(Image, love::GRAPHICS_IMAGE_T);
@@ -21,10 +23,37 @@ WRAP_FUNCTION(getHeight)
     return JSValueMakeNumber(ctx, height);
 }
 
+WRAP_FUNCTION(setFilter)
+{
+    Image* image = JSLExtractObject<Image>(ctx, self);
+    if (image and argCount == 2) {
+        std::string min = JSLGetString(ctx, args[0]);
+        std::string mag = JSLGetString(ctx, args[1]);
+
+        // Set filter mode.
+        Image::Filter f;
+
+        if (not Image::getConstant(min.c_str(), f.min)) {
+            printf("ERROR: Invalid min filter: %s\n", min.c_str());
+            goto undefined;
+        }
+
+        if (not Image::getConstant(mag.c_str(), f.mag)) {
+            printf("ERROR: Invalid mag filter: %s\n", mag.c_str());
+            goto undefined;
+        }
+
+        image->setFilter(f);
+    }
+
+undefined:
+    return JSValueMakeUndefined(ctx);
+}
+
 WRAP_CLASS(Image) {
     JSLAddFunction(ctx, self, "getWidth", getWidth);
     JSLAddFunction(ctx, self, "getHeight", getHeight);
-    //setFilter
+    JSLAddFunction(ctx, self, "setFilter", setFilter);
     //getFilter
     //setWrap
     //getWrap
